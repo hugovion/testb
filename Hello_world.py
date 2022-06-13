c263ec7c818d36da7e7c3f248f44dcc2cb9c0cd5
hugovion <h.vion28@gmail.com>
2022-06-13 15:21:24 +0200

    edit

---
+++ Hello_world.py
@@ -0,0 +1,13 @@
+#test
+# coding: utf-8
+
+# This file produce some printing
+
+print("Hello world")
+
+
+# We introduce some interaction with the user
+
+name = input("What's your name?")
+print("\nHello " + name)
+
